fd0840e60ffe11480e2679d7331323cdb3c4462f
Jimmy Jurado <jurado140@gmail.com>
2023-02-22 16:49:11 -0500

    Add variables to the file

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+member_names = ["jimmy", "john", "justin", "jason"]
+ages = ["31", "35", "28", "55"]
+weights = ["220.50", "195.75", "157.60", "175.00"]
+current_member - ["true", "false", "false", "true"]
